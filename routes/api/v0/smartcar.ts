f244d893081db2247a4d6e6a3da4d0426dd07a47
Cody Hatfield <cody.hatfield@me.com>
2022-05-02 14:50:36 -0700

    Encrypt token endpoint

--- routes/api/v0/smartcar.ts
+++ routes/api/v0/smartcar.ts
@@ -2,9 +2,28 @@ import { Router } from "express";
 import "dotenv/config";
 import * as smartcar from "smartcar";
 import Moralis from "moralis/node";
+import { Resolver } from "did-resolver";
+import WebResolver from "web-did-resolver";
+import KeyResolver from "key-did-resolver";
+import { DID } from "dids";
+import { fromString } from "uint8arrays/from-string";
+import { Ed25519Provider } from "key-did-provider-ed25519";
 
 const smartcarClient = new smartcar.AuthClient({
   testMode: true,
+});
+
+const webResolver = WebResolver.getResolver();
+const keyResolver = KeyResolver.getResolver();
+const seed = fromString(process.env.KEY_SEED!, "base16");
+const provider = new Ed25519Provider(seed);
+
+const did = new DID({
+  provider,
+  resolver: new Resolver({
+    ...webResolver,
+    ...keyResolver,
+  }),
 });
 
 var router = Router();
@@ -43,5 +62,59 @@ router.post("/authorize", async (req: any, res: any, next: any) => {
     next(err);
   }
 });
+
+router.get(
+  "/vehicles/:vehicleId/token",
+  async (req: any, res: any, next: any) => {
+    // TODO: Authentication
+
+    try {
+      await Moralis.start({
+        serverUrl: process.env.MORALIS_SERVER_URL,
+        appId: process.env.MORALIS_APP_ID,
+        masterKey: process.env.MORALIS_MASTER_KEY,
+      });
+
+      const vehicleId = req.params.vehicleId;
+      const Vehicle = Moralis.Object.extend("Vehicle");
+      const query = new Moralis.Query(Vehicle);
+      query.equalTo("vehicleId", vehicleId);
+      const results = await query.find();
+
+      if (results.length == 0) {
+        res.sendStatus(404);
+        return;
+      }
+
+      const vehicle = results[0];
+      let accessToken = vehicle.get("accessToken");
+
+      try {
+        const scVehicle = new smartcar.Vehicle(vehicleId, accessToken);
+        await scVehicle.permissions();
+      } catch (err) {
+        const refreshToken = vehicle.get("refreshToken");
+        const tokens = await smartcarClient.exchangeRefreshToken(refreshToken);
+
+        accessToken = tokens.accessToken;
+
+        vehicle.set("accessToken", tokens.accessToken);
+        vehicle.set("refreshToken", tokens.refreshToken);
+        vehicle.set("refreshExpiration", tokens.refreshExpiration);
+        await vehicle.save();
+      }
+
+      const scVehicle = new smartcar.Vehicle(vehicleId, accessToken);
+      await scVehicle.permissions();
+
+      await did.authenticate();
+      const jwe = await did.createDagJWE(accessToken, [did.id]);
+
+      res.json({ encryptedToken: jwe });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
